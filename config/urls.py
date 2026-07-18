091d58c253f487f620b4e8e5674bdc1c0c67a90f
minotaur-mmnana_pc <thestreets@hotmail.co.th>
2026-07-19 01:22:41 +0700

    Fixing bug to run on local server (Raspberry pi 4 x64 OS lite)

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from portfolio.forms import BootstrapAuthenticationForm
 
 urlpatterns = [
@@ -29,7 +29,5 @@ urlpatterns = [
         authentication_form = BootstrapAuthenticationForm,
     ), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(next_page = 'login'), name = 'logout'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

155421a35e510885aa1d0f698247795c0c89d0bb
minotaur-mmnana_pc <thestreets@hotmail.co.th>
2026-07-20 21:06:42 +0700

    Add new home page.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.LotListView.as_view(), name = 'lot_list'),
+    path('', views.HomePortfolioListView.as_view(), name = 'home'),
     path('buy/', views.StockLotCreateView.as_view(), name = 'lot_create'),
     path('sell/', views.SellView.as_view(), name = 'sell_create'),
     path('sales/', views.SaleListView.as_view(), name = 'sale_list'),
     path('lots/<int:pk>/evidence/', views.LotEvidenceView.as_view(), name = 'lot_evidence'),
     path('sales/<int:pk>/evidence/', views.SaleEvidenceView.as_view(), name = 'sale_evidence'),
     path('reports/fifo/', views.PortfolioReportView.as_view(), name = 'fifo_report'),
-    path('home/', views.HomePortfolioListView.as_view(), name = 'home'),
+    # path('home/', views.HomePortfolioListView.as_view(), name = 'home'),
+    path('lots/', views.LotListView.as_view(), name = 'lot_list'),
 ]
